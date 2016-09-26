30c45b02baeb44b96fb03e6420f621d1eb6920df
Sylvestre Gug <sylvestre@outriggermedia.com>
2016-09-26 18:05:21 -0400

    Added layout and container

--- public/src/js/ngD3Demo.controllers.js
+++ public/src/js/ngD3Demo.controllers.js
@@ -1,6 +1,5 @@
 var ngD3DemoControllers = angular.module('ngD3Demo.controllers', []);
 
-ngD3DemoControllers.controller('home',['$scope',function($scope){
+ngD3DemoControllers.controller('index',['$scope',function($scope){
 	$scope.title = "d3 angular charts";
-
 }]);
